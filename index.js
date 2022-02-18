f8e82de1a0a548763fbe0d1e04d7fa3fc319d17f
sum-native <74903277+sum-native@users.noreply.github.com>
2022-02-18 12:25:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const qs = require("querystring");
+const url = require('url');
+
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+
+    if (req.method !== 'GET') handleError(405, res);
+
+    const {pathname, query} = url.parse(req.url);
+
+    if (pathname === '/name') {
+        const {name} = qs.parse(query);
+        if (name) {
+            res.end(`<h1>You Entered: ${name.toUpperCase()}</h1>`)
+        } else {
+            res.end("Bad Request");
+        }
+        
+    }
+
+    if (pathname === '/') {
+        res.end(`<h1>Hello Worldddd</h1>`)
+    }
+});
+
+function handleError(code, res) {
+    res.statusCode = code;
+    res.end(`{"error": "${http.STATUS_CODES[code]}"}`);
+}
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
